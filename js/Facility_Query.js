49a97db36d9391c7f47819fdf1ef352d33394685
Delreyaa <1251455694@qq.com>
2020-06-13 20:52:50 +0800

    01基本功能实现

---
+++ js/Facility_Query.js
@@ -0,0 +1,92 @@
+var quanjuobj;
+var quanjulist = [];
+var e = 0;
+var oo = L.geoJSON();
+var YYY, XXX;
+var startx, starty;
+var endx, endy;
+var myIconstart;
+var route = L.geoJSON();
+var arr;
+var listt = [];
+var routepng;
+var layertest = [];
+var myGrouptest = L.layerGroup();
+var myGroupstart = L.layerGroup();
+var myGroupend = L.layerGroup();
+var kk, kkk;
+var carName = [];
+var bn = 0;
+$(function () {
+    $("[data-toggle='tooltip']").tooltip();
+});
+
+// 控制左边菜单栏滑入滑出
+$('#btn').click(function () {
+    if (n == 1) {
+        $('#test').animate({
+            'margin-left': '0px'
+        },
+            250);
+        $('#map').animate({},
+            2500);
+        n = n - 1;
+
+    } else {
+        $('#test').animate({
+            'margin-left': '-300px'
+        },
+            250);
+        $('#map').animate({
+            'margin-left': '50px'
+        },
+            350);
+        n = n + 1;
+    }
+});
+
+var n = 0;
+var w1, w2, w3, w4, c1, c2, c3, c4;
+var handler1 = 'null';
+var handler2 = 'null';
+var handler3 = 'null';
+var objid = [];
+var ally;
+var newresultLayer = 'null';
+var allresultLayer = 'null';
+var editableLayerss;
+
+var map, url_map = "http://localhost:8090/iserver/services/map-EmergWS/rest/maps/EmergMap";     //地图地址
+// 初始化地图信息
+//!!!地图要经过坐标转换！
+var map = L.map('map', {        //地图窗口
+    crs: L.CRS.EPSG4326,
+    center: [30.61, 103.7],
+    zoom: 13,
+    minzoom: 10,
+    maxzoom: 18
+});
+
+L.supermap.tiledMapLayer(url_map).addTo(map);       //添加地图
+
+// 创建图层组
+var editableLayers = new L.FeatureGroup();
+map.addLayer(editableLayers);
+$("#map").css("position", "sticky");
+// 监听绘制
+map.on(L.Draw.Event.CREATED, completed);
+
+// 地图浏览操作基本功能
+function Fullwidth() {   //全图
+    var latlng = L.latLng(30.61, 103.7);
+    map.setView([30.61, 103.7], 13);
+}
+function enlarge() {     //放大
+    map.zoomIn();
+}
+function narrow() {      //缩小
+    map.zoomOut();
+}
+function translation() { //平移
+    map.panTo([30.61, 103.71]);
+}
